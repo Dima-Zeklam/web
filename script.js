fdc245a64a7e35cae36e8f5cbe4423555f2fc3b0
DimaZiklam <>
2021-06-03 10:29:20 +0000

    add js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+console.log("it's work");
+  var d = new Date();
+         
+confirm("Today is " +  d  );
+var name =prompt("what is your name ? ");
+
+if(name !=  null)
+  alert("Hi " + name + ", welcom to my page ^_^");
+
+var color =prompt("what is your favourit color ? ");
+
+if(color != null){
+ document.getElementById("change").style.color=color;
+}
+
+
